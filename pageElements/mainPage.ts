076508f90bec218a6013740ebf0e34d1870da51c
Kevin <reynderskev@gmail.com>
2025-11-25 12:25:49 +0100

    Log out flow

--- pageElements/mainPage.ts
+++ pageElements/mainPage.ts
@@ -2,6 +2,7 @@ export const mainPage = {
     loginButton: 'text=Inloggen',
     userDropdown: '.btn btn--icon btn--naked user-dropdown__button',
     logoutButton: '//*[@id="tertiaryNavUserMenu"]/div[2]/ul/li[3]/a',
+    logoutConfirmButton: '[data-testid="logout-popup-logout"]',
     cookiesAcceptButton: '#pg-accept-btn',
     submitButton: '[type=submit]'
 }
--- tests/loginlogoutflow.spec.ts
+++ tests/loginlogoutflow.spec.ts
@@ -1,31 +1,31 @@
 import { test, expect } from '@playwright/test';
-import { loginPage } from '../pageElements/login';
-import { mainPage } from '../pageElements/main';
+import { loginPage } from '../pageElements/loginPage';
+import { mainPage } from '../pageElements/mainPage';
+import { acceptCookiesIfPresent } from '../fixtures/mainFixture';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.vtmgo.be');
+  await acceptCookiesIfPresent(page);
+});
 
 test('Logging in on VTM GO', async ({ page }) => {
   await page.goto('https://www.vtmgo.be');
-  await page.waitForTimeout(3000);
-  //cookies
-  try {
-    await page.click(mainPage.cookiesAcceptButton, { timeout: 3000 });
-  } catch (e) {
-  //If cookies are not found, continue
-  }
-  await page.waitForTimeout(3000);
   await page.click(mainPage.loginButton); //Click login button
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(5000); //Hardcoded a wait at the moment for loading, replacing later with proper check
   await page.fill(loginPage.username, 'reynderskev@gmail.com'); //Fill in email
-  await page.waitForTimeout(3000);
   await page.locator(loginPage.submitButton).click(); //Click Ga verder ->
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(5000);
   await page.fill(loginPage.password, 'Testingskills!1'); //Fill in password
-  await page.waitForTimeout(3000);
   await page.locator(loginPage.submitButton).click(); //Click Log in ->
   // Continue with further checks as needed
 });
 
 test('Logging out on VTM GO', async ({ page }) => {
+  //I've tried using an auth file, so basically having a logged-in state here, but vtmgo blocks automated logins, which blocks this flow
   await page.goto('https://www.vtmgo.be/');
-
-
+  await page.click(mainPage.userDropdown);
+  await page.click(mainPage.logoutButton);
+  await page.click(mainPage.logoutConfirmButton);
+  await expect(page.locator(mainPage.loginButton)).toBeVisible();
+  // Continue with further checks as needed
 });

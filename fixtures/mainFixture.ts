f2d5d85c83946b64bc2dcbd8a844cb65964bc2ba
Kevin <reynderskev@gmail.com>
2025-11-26 10:29:44 +0100

    Added a comment regarding use of a locator

--- fixtures/mainFixture.ts
+++ fixtures/mainFixture.ts
@@ -8,4 +8,8 @@ export async function acceptCookiesIfPresent(page: Page) {
     } catch (e) {
         // If cookies are not found, continue
     }
+
+    /*To accept the cookies, I could also use await page.getByRole('button', { name: 'Akkoord' }).click();
+    However, this is less reusable as it is language dependent. (Complete site is in Dutch only, but still)
+    Therefore, I use the very specific selectors to be independent from the language need.*/
 }

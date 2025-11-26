cd2af448c7ed536096a831946c8628b9034aa231
Kevin <reynderskev@gmail.com>
2025-11-26 11:46:40 +0100

    More cleanup

--- fixtures/accountFixture.ts
+++ fixtures/accountFixture.ts
@@ -4,12 +4,12 @@ import { loginPage } from '../pageElements/loginPage';
 import { loginCredentials } from '../accounts/credentials';
 
 export async function login(page: Page) {
-    await page.click(mainPage.loginButton);
-    await page.fill('input[name="username"]', loginCredentials.username); 
-    await page.click(loginPage.submitButton);
+    await page.click(mainPage.loginButton); //Click login button
+    await page.fill('input[name="username"]', loginCredentials.username); //Fill in email
+    await page.click(loginPage.submitButton); //Click Ga verder ->
     //Not getting further here without being blocked
-    await page.fill('input[name="password"]', loginCredentials.password);
-    await page.click(loginPage.submitButton);
+    await page.fill('input[name="password"]', loginCredentials.password); //Fill in password
+    await page.click(loginPage.submitButton); //Click Log in ->
     await page.waitForResponse('https://login.vtmgo.be/ssosession'); //Contains a sessionid 0 when logged out, a unique id when logged in
     await expect(page.locator(mainPage.userDropdown)).toBeVisible(); //To check if you're actually logged in based on something that only shows when logged in
 }
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,6 +1,7 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 import { acceptCookiesIfPresent } from '../fixtures/mainFixture';
+import { login } from '../fixtures/accountFixture';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
@@ -8,12 +9,6 @@ setup('authenticate', async ({ page }) => {
   const response = await page.goto('https://www.vtmgo.be/vtmgo'); //Specifically referring to this page for login flow
   await page.waitForTimeout(3000);
   await acceptCookiesIfPresent(page);
-  await page.click('text=Inloggen'); //Click login button
-  await page.fill('input[name="username"]', 'reynderskev@gmail.com'); //Fill in email
-  await page.locator("[type=submit]").click(); //Click Ga verder ->
-  await page.fill('input[name="password"]', 'Testingskills!1'); //Fill in password
-  await page.locator("[type=submit]").click(); //Click Log in ->
-  await page.waitForResponse('https://login.vtmgo.be/ssosession'); //Wait for the login response to ensure login is complete
-
+  await login(page);
   await page.context().storageState({ path: authFile });
 });
